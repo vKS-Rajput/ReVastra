0799d9a89d71d7a65b061b1cc3804e3679c9ce86
vKS-Rajput <k8659653@gmail.com>
2025-01-26 13:37:43 +0530

    Delivery Fee Fix

--- frontEnd/src/context/ShopContext.jsx
+++ frontEnd/src/context/ShopContext.jsx
@@ -7,7 +7,7 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
     const currency = "₹";
-    const delivery_fee = 10;
+    const delivery_fee = getCartAmount*0.02;
     const backEndURL = import.meta.env.VITE_BACKEND_URL;
 
     // State management
