3f3932d260fda75db9c2269bfc0ba87d0c13f778
vKS-Rajput <k8659653@gmail.com>
2025-01-31 20:19:43 +0530

    Order Route Fix

--- backEnd/routes/orderRoute.js
+++ backEnd/routes/orderRoute.js
@@ -10,12 +10,12 @@ orderRouter.post('/list',adminAuth,allOrders)
 orderRouter.post('/status',adminAuth,updateStatus)
 
 // Payment Features
-orderRouter.post('/place',placeOrder)
+orderRouter.post('/place',authUser ,placeOrder)
 
 // orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
 
 // User Feature 
-orderRouter.post('/userorders',userOrder)
+orderRouter.post('/userorders',authUser,userOrder)
 
 // verify payment
 
