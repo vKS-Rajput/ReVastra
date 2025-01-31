3dac9f57e8fa1eb6802c5cceea9c23cab9a80e43
vKS-Rajput <k8659653@gmail.com>
2025-01-31 19:23:14 +0530

    sd

--- backEnd/models/orderModel.js
+++ backEnd/models/orderModel.js
@@ -9,7 +9,7 @@ const orderSchema = new mongoose.Schema({
     status: {type: String, required: true, default: 'Order Placed'},
     paymentMethod: {type: String, required: true},
     payment: {type: Boolean, required:true, default: false},
-    date: {type: Number, required: true, }
+    date: {type: Number }
 })
 
 const orderModel = mongoose.models.order || mongoose.model('order', orderSchema)
