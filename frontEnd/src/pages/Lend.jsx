1b06fa94aecc7b885cec542f58c1e85938947ab5
vKS-Rajput <k8659653@gmail.com>
2025-02-04 09:41:52 +0530

    Lend Fixed

--- frontEnd/src/pages/Lend.jsx
+++ frontEnd/src/pages/Lend.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { backEndURL } from "../App";
 
 const Lend = ({ token, backEndURL }) => {
   const [images, setImages] = useState([null, null, null, null]);
