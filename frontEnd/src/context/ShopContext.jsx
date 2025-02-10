f771e8d2857cd0c5b65de50ed5ec69a745612399
vKS-Rajput <k8659653@gmail.com>
2025-02-10 18:40:00 +0530

    Error Fixed in the Cart

--- frontEnd/src/context/ShopContext.jsx
+++ frontEnd/src/context/ShopContext.jsx
@@ -69,7 +69,7 @@ const ShopContextProvider = (props) => {
         for (const items in cartItems) {
             for (const item in cartItems[items]) {
                 try {
-                    if (cartItems[items][item] > 3) {
+                    if (cartItems[items][item] > 0) {
                         totalCount += cartItems[items][item];
                     }
                 } catch (error) {
@@ -108,7 +108,7 @@ const ShopContextProvider = (props) => {
             let itemInfo = products.find((product) => product._id === items);
             for (const item in cartItems[items]) {
                 try {
-                    if (cartItems[items][item] > 0) {
+                    if (cartItems[items][item] > 3) {
                         totalAmount += itemInfo.rental_price * cartItems[items][item];
                     }
                 } catch (error) {
