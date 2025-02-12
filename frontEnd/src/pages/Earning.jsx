1fa1a1566f209f6435324d5614e2e87f9662f0a9
vKS-Rajput <k8659653@gmail.com>
2025-02-12 22:15:43 +0530

    Earning Page Under Development

--- frontEnd/src/pages/Earning.jsx
+++ frontEnd/src/pages/Earning.jsx
@@ -1,43 +1,19 @@
-import React, { useContext, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import Title from '../components/Title'
-import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
 
 const Earning = () => {
+    return (
+        <div className='border-t pt-24 bg-gray-50 min-h-screen flex flex-col items-center justify-center'>
+            <div className='text-2xl text-center font-semibold text-gray-800'>
+                <Title text1={'MY'} text2={'Earning'} />
+            </div>
 
-    const {backEndURL, token, currency} = useContext(ShopContext)
-    const [earningData, setEarningData] = useState([]);
-
-    const loadEarningData = async() => {
-        try {
-            if(!token){
-                return null;
-            }
-            const response = await axios.post(backEndURL + '/api/order/my_earning', {}, {headers: {token}});
-            if (response.data.success) {
-                let allOrderEarning = [];
-                response.data.earnings.map((earning) => {
-                    earning.items.map((item) => {
-                        item['']
-                    })
-                })
-            }
-        } catch (error) {
-            
-        }
-    }
-  return (
-    <div className='border-t pt-24 bg-gray-50'>
-        <div className='text-2xl text-center font-semibold text-gray-800'>
-            <Title text1={'MY'} text2={'Earning'}/>
+            <div className='mt-8 text-lg text-gray-600 font-medium'>
+                🚧 This page is under development. Stay tuned! 🚀
+            </div>
         </div>
+    );
+};
 
-        <div className=' mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-3 sm:gap-5'>
-
-        </div>
-        
-    </div>
-  )
-}
-
-export default Earning
+export default Earning;
