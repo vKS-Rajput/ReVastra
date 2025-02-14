c65374860e61b5a40b638e64d04f86e061d0a2f4
vKS-Rajput <k8659653@gmail.com>
2025-02-14 15:15:39 +0530

    GoogleLend Paged Updated

--- frontEnd/src/pages/GoogleLend.jsx
+++ frontEnd/src/pages/GoogleLend.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const GoogleLend = () => {
   return (
-    <div>GoogleLend</div>
-  )
-}
+    <div>
+      <h2>GoogleLend</h2>
+      <iframe 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeT5eiV3spSwB_HnxtwCUcOFlkViLCLMAKYFovsFrz93EaCaQ/viewform?embedded=true" 
+        width="640" 
+        height="2041" 
+        frameBorder="0" 
+        marginHeight="0" 
+        marginWidth="0"
+        title="Google Lend Form"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+};
 
-export default GoogleLend
+export default GoogleLend;
