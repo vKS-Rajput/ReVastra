608e12ba2827ce17163363efa1c620aa4bc28606
vKS-Rajput <k8659653@gmail.com>
2025-02-14 15:30:20 +0530

    Google lend Design Change

--- frontEnd/src/pages/GoogleLend.jsx
+++ frontEnd/src/pages/GoogleLend.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GoogleLend = () => {
   return (
-    <div>
+    <div style={{ marginTop: '20px', textAlign: 'center' }}>
       <h2>GoogleLend</h2>
       <iframe 
         src="https://docs.google.com/forms/d/e/1FAIpQLSeT5eiV3spSwB_HnxtwCUcOFlkViLCLMAKYFovsFrz93EaCaQ/viewform?embedded=true" 
@@ -12,6 +12,7 @@ const GoogleLend = () => {
         marginHeight="0" 
         marginWidth="0"
         title="Google Lend Form"
+        style={{ borderRadius: '8px', boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }}
       >
         Loading…
       </iframe>
